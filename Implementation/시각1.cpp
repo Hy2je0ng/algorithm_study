1480da8523c33d721d17c0e140e4d191439c5659
hy2je0ng <hy2je0ng@naver.com>
2021-02-01 20:25:28 +0900

    [upload] 시각 cpp 코드

---
+++ Implementation/시각1.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+
+int getDecimal(int n);
+
+int getDigit(int n);
+
+int main(){
+	
+	int n;
+	int result = 0;
+	
+	scanf("%d", &n);
+	
+	for(int i = 0; i <= n; i++){ // ½Ã 
+		
+		if(getDecimal(i) == 3 || getDigit(i) == 3){
+			result += 60 * 60;
+			continue;
+		}
+
+		for(int j = 0; j < 60; j++){ // ºÐ 
+		
+			if(getDecimal(j) == 3 || getDigit(j) == 3){
+				result += 60;
+				continue;
+			}
+					
+			for(int k = 0; k < 60; k++){ // ÃÊ 
+				
+				if(getDecimal(k) == 3 || getDigit(k) == 3){
+					result += 1;
+					continue;
+				}	
+			}	
+		}
+	}
+	
+	printf("%d", result);
+	
+	return 0;
+}
+
+int getDecimal(int number){
+	return number / 10;
+}
+
+int getDigit(int number){
+	return number % 10;
+}
