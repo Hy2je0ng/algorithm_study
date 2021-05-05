c026ef14f64f5511f006e3565d0c444c025b78be
hy2je0ng <hy2je0ng@naver.com>
2021-05-05 14:17:41 +0900

    [upload] CountSort cpp Code

---
+++ Sort/CountSort.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+#define MAX_VALUE 9
+ 
+using namespace std;
+
+int main() {
+
+	int n = 15;
+	int data[n] = {0, };
+	int cnt[MAX_VALUE] = {0,};
+	
+	// ==== test data ====
+	// 7 5 9 0 3 1 6 2 9 1 4 8 0 5 2
+
+	for (int i = 0; i < n; i++) {
+		scanf("%d", &data[i]);
+	}
+
+	for (int i = 0; i < n; i++) {
+		cnt[data[i]] += 1;
+	}
+	
+	for (int i = 0; i < MAX_VALUE; i++) {
+		for(int j = 0; j < cnt[i]; j++){
+			printf("%d ", i);
+		}
+	}
+
+	return 0;
+}
