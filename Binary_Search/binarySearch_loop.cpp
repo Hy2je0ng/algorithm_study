05a905109ec99b827841867abde4d18951def821
hy2je0ng <hy2je0ng@naver.com>
2021-05-06 22:13:27 +0900

    Upload BinarySearch cpp code

---
+++ Binary_Search/binarySearch_loop.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int binarySearch(vector<int> &v, int target, int start, int end){
+	
+	while(start <= end){
+		int mid = (start + end) / 2;
+		
+		if(target == v[mid]) return mid;
+		else if(target < v[mid]) end = mid - 1;
+		else if(target > v[mid]) start = mid + 1;
+	}
+	return -1;
+	
+}
+
+int main(){
+	
+	int n, target;
+	vector<int> v;
+	
+//   ==== test data ====
+//	 10 75
+//	 0 11 22 31 45 51 60 75 82 99
+	
+	scanf("%d %d", &n, &target);
+	
+	for (int i = 0; i < n; i++) {
+		int x;
+		scanf("%d", &x);
+		v.push_back(x);
+	}
+	
+	sort(v.begin(), v.end()); // 이진 탐색을 위해 정렬 
+	
+	int result = binarySearch(v, target, 0, n-1);
+	
+	if (result == -1) {
+		printf("원소가 존재하지 않습니다.\n");
+    }
+    else {
+    	printf("%d\n", result + 1);
+    }
+	
+	return 0;
+}
